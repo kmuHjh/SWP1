745fabe77a94e720f495e8174c6eda620f1df8db
kmuHjh <gkwjdgus42@gmail.com>
2020-06-27 01:57:42 +0900

    issue #3 edit fig.py

--- calc/fig.py
+++ calc/fig.py
@@ -5,18 +5,12 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	a = d.get('a', [''])[0]
 	b = d.get('b', [''])[0]
-	if a=='':
-		a=0
-	else:
-		a = int(a)
-	if b=='':
-		b=0
-	else:
-		b = int(b)
-	response_body = html % {
-		'sum' : a+b,
-		'mul' : a*b,
-	}
+	sum, mul = 0, 0
+	if '' not in [a, b]:
+		a, b = int(a), int(b)
+		sum = a + b
+		mul = a * b
+	response_body = html % {'sum':sum, 'mul':mul}
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
 		('Content-Length', str(len(response_body)))
