e0d11df1747b6d28d1dee3e14f68787170a5b172
kmuHjh <gkwjdgus42@gmail.com>
2020-06-27 01:31:26 +0900

    add wsgi homework file

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+	<body>
+		<form method="GET"  action="">
+			x = <input type="number" name="a"><br>
+			y = <input type="number" name="b"><br>
+			<input type="submit">
+		</form>
+		<p>
+		sum : %(sum)i<br>
+		mul : %(mul)i<br>
+		</p>
+	</body>
+</html>
+"""
